0a840fe4cde14e839bdb9d03390c9eb438b1d6b8
Timothy Nordloh <tnordloh@gmail.com>
2018-05-19 18:58:49 -0600

    fix setup script...

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,16 +1,16 @@
 
 cd ~/.Vim/bundle
-git checkout https://github.com/scrooloose/nerdtree.git
-git checkout https://github.com/leafgarland/typescript-vim.git
-git checkout git://github.com/SirVer/ultisnips.git
-git checkout git://github.com/tpope/vim-bundler.git
-git checkout https://github.com/kchmck/vim-coffee-script.git
-git checkout git://github.com/tpope/vim-commentary.git
-git checkout https://github.com/elixir-lang/vim-elixir.git
-git checkout git://github.com/tpope/vim-fugitive.git
-git checkout git://github.com/digitaltoad/vim-pug.git
-git checkout git://github.com/tpope/vim-rails.git
-git checkout git://github.com/vim-ruby/vim-ruby.git
-git checkout git://github.com/tpope/vim-sleuth.git
-git checkout https://github.com/honza/vim-snippets.git
-git checkout https://github.com/benmills/vimux
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/leafgarland/typescript-vim.git
+git clone git://github.com/SirVer/ultisnips.git
+git clone git://github.com/tpope/vim-bundler.git
+git clone https://github.com/kchmck/vim-coffee-script.git
+git clone git://github.com/tpope/vim-commentary.git
+git clone https://github.com/elixir-lang/vim-elixir.git
+git clone git://github.com/tpope/vim-fugitive.git
+git clone git://github.com/digitaltoad/vim-pug.git
+git clone git://github.com/tpope/vim-rails.git
+git clone git://github.com/vim-ruby/vim-ruby.git
+git clone git://github.com/tpope/vim-sleuth.git
+git clone https://github.com/honza/vim-snippets.git
+git clone https://github.com/benmills/vimux
